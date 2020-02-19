ea849c33ec125d6d0a2eafaa29a7071b031f1499
SCMEVan <vansup111@gmail.com>
2020-02-19 11:16:59 +0300

    Программа для  пересохранения профилей перед обновлением кипа, запускается 1 раз на сервере (пароль удалил из строки подключения)

---
+++ BaseRepair/Program.cs
@@ -0,0 +1,62 @@
+﻿using SCME.InterfaceImplementations.NewImplement.MSSQL;
+using SCME.InterfaceImplementations.NewImplement.SQLite;
+using SCME.Types;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            var q = new SqlConnectionStringBuilder()
+                {
+                    DataSource = @"tcp:192.168.0.134, 1444",
+                    InitialCatalog = @"SCME_ResultsDB",
+                    IntegratedSecurity = false,
+                    UserID = "sa",
+                    Password = ,
+                    ConnectTimeout = 15
+                };
+            var  mssqlDbService = new MSSQLDbService(new  SqlConnection(q.ToString()));
+
+            var msSqlDbServiceProxy = new DatabaseProxy("SCME.CentralDatabase");
+
+            var centralProfiles = msSqlDbServiceProxy.GetProfilesDeepByMmeCode("MME007");
+            var centralProfiles1 = msSqlDbServiceProxy.GetProfilesDeepByMmeCode("MME007");
+
+            
+                foreach (var i in centralProfiles1)
+                {
+                    i.Version ++;
+                    i.Timestamp = DateTime.Now;
+                    i.Key = Guid.NewGuid();
+                    i.Id = 0;
+                }
+                var w = centralProfiles.Zip(centralProfiles1, (m,n) => new {oldP = m, newP = n  }).ToList();
+
+            int qwe = 0;
+
+                foreach (var i in w)
+                {
+                    mssqlDbService.InsertUpdateProfile(i.oldP, i.newP, "MME007");
+                qwe++;
+                }
+            }
+            catch(Exception ex)
+            {
+                File.WriteAllText("error.txt", ex.ToString());
+            }
+
+        }
+    }
+}
