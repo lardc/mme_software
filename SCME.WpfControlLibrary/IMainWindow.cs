6809f2a0e0e9de1cb533270a247e8d65e4927523
SCMEVan <vansup111@gmail.com>
2020-03-17 16:39:01 +0300

    BF 37

--- SCME.WpfControlLibrary/IMainWindow.cs
+++ SCME.WpfControlLibrary/IMainWindow.cs
@@ -1,9 +1,10 @@
-﻿using SCME.WpfControlLibrary.CustomControls;
+﻿using System.Windows.Controls;
+using SCME.WpfControlLibrary.CustomControls;
 
 namespace SCME.WpfControlLibrary
 {
     public interface IMainWindow
     {
-        void ShowKeyboard(bool Show, ValidatingTextBox Control);
+        void ShowKeyboard(bool Show, Control Control);
     }
 }
