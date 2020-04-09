9c9d15883f3b5edf46d5173ec4b4610ce6621c47
SCMEVan <vansup111@gmail.com>
2020-04-09 16:43:02 +0300

    Implement float parsing patch, clamp default height set 0, config ignore from commit

--- SCME.ProfileBuilder/App.xaml.cs
+++ SCME.ProfileBuilder/App.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using SCME.ProfileBuilder.Properties;
 using System.Globalization;
+using System.IO;
 using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Threading;
 using Application = System.Windows.Application;
 
 namespace SCME.ProfileBuilder
@@ -14,9 +16,15 @@ namespace SCME.ProfileBuilder
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
             Thread.CurrentThread.CurrentCulture = new CultureInfo(Settings.Default.CurrentCulture);
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(Settings.Default.CurrentCulture);
             FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentUICulture.IetfLanguageTag)));
+        }
+
+        private static void Current_DispatcherUnhandledException(object Sender, DispatcherUnhandledExceptionEventArgs E)
+        {
+            File.WriteAllText("error.txt", E.Exception.ToString());
         }
     }
 }
--- SCME.UpdateServer/UpdateDataConfig.cs
+++ SCME.UpdateServer/UpdateDataConfig.cs
@@ -18,11 +18,8 @@ namespace SCME.UpdateServer
         public string DataPathRoot { get; set; }
         public string ScmeAgentFolderName { get; set; }
         public string ScmeAgentExeName { get; set; }
-        
-        public string ScmeServiceConfigName { get; set; }
-        
+
         public string ScmeCommonConfigName { get; set; }
-        public string ScmeUiConfigName { get; set; }
 
         public string ScmeUIExeName { get; set; }
         public MmeParameter[] MmeParameters { get; set; }
