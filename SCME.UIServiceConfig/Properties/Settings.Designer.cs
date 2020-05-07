2b4bc9c8dafaa12c8769887e7e8d27075e7463e0
SCMEVan <vansup111@gmail.com>
2020-05-07 14:54:27 +0300

    Jet brains setting designer bf

--- SCME.UIServiceConfig/Properties/Settings.Designer.cs
+++ SCME.UIServiceConfig/Properties/Settings.Designer.cs
@@ -13,11 +13,11 @@ namespace SCME.UIServiceConfig.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-    public sealed partial class Settings1 : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings1 defaultInstance = ((Settings1)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings1())));
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
-        public static Settings1 Default {
+        public static Settings Default {
             get {
                 return defaultInstance;
             }
