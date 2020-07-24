99a8688f11d161f3ee83b3a5d09f1cf2a0338a2d
SCMEVan <vansup111@gmail.com>
2020-07-24 15:46:04 +0300

    build files

--- build/Buildx64Minimal.bat
+++ build/Buildx64Minimal.bat
@@ -1,6 +1,6 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" ..\SCME.Service\SCME.Service.csproj /restore /t:Rebuild /p:OutputPath="..\publish\UIService" /p:Configuration=Debug /p:WarningLevel=0 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" ..\SCME.DatabaseServer\SCME.DatabaseServer.csproj /restore /t:Rebuild /p:OutputPath="..\publish\Server" /p:Configuration=Debug /p:WarningLevel=0 
-dotnet publish ..\SCME.UI\SCME.UI.csproj -c Debug -o ..\publish\UIService\UI --self-contained true -r win-x86
+dotnet publish ..\SCME.UI\SCME.UI.csproj -c Debug -o ..\publish\UIService\UI --self-contained false -r win-x64
 GetVersion.bat ..\publish\UIService\UI\SCME.UI.exe ..\publish\UIService\UI\Version.txt
 dotnet build-server shutdown
 pause
