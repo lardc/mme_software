59569375ce124b5e830cbbaf529b50479f63b33f
Evgeny Minlibaev <evgenyminlibaev@gmail.com>
2021-04-26 23:55:26 +0500

    * Редизайн страницы ввода пароля * Редизайн страницы профилей с экспандером * Редизайн иконок формы

--- SCME.UI/PagesUser/LoginPage.xaml.cs
+++ SCME.UI/PagesUser/LoginPage.xaml.cs
@@ -1,149 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SCME.Types;
+using SCME.UIServiceConfig.Properties;
+using SCME.WpfControlLibrary.CustomControls;
+using SCME.WpfControlLibrary.Pages;
+using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using SCME.Types;
-using SCME.Types.Profiles;
-using SCME.UI.IO;
-using SCME.UIServiceConfig.Properties;
-using SCME.WpfControlLibrary;
-using SCME.WpfControlLibrary.CustomControls;
-using SCME.WpfControlLibrary.Pages;
+using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace SCME.UI.PagesUser
 {
-    /// <summary>
-    ///     Interaction logic for LoginPage.xaml
-    /// </summary>
+    /// <summary>Страница авторизации</summary>
     public partial class LoginPage
     {
-        private CAccount CurrentAccount { get; set; }
+        //Анимация загрузки окна
+        private LoadingAnimationWindow LoadingAnimationWindow;
 
+        /// <summary>Инициализирует новый объект класса LoginPage</summary>
         public LoginPage()
         {
             InitializeComponent();
-
             CurrentAccount = new CAccount();
-
+            //Подгрузка списка пользователей
             accountsListBox.ItemsSource = new AccountEngine(Settings.Default.AccountsPath).Collection;
-
-            if (accountsListBox.Items.Count > 0)
-                accountsListBox.SelectedIndex = 0;
-            else
+            //Список пользователей пуст
+            if (accountsListBox.Items.Count == 0)
                 buttonNext.Visibility = Visibility.Hidden;
+            else
+                accountsListBox.SelectedIndex = 0;
         }
 
-        private void btBack_OnClick(object sender, RoutedEventArgs e)
+        /// <summary>Текущий пользователь</summary>
+        private CAccount CurrentAccount
+        {
+            get; set;
+        }
+
+        private void LoginPage_Loaded(object sender, RoutedEventArgs e) //Загрузка окна
+        {
+            lblIncorrect.Content = string.Empty;
+        }
+
+        private void BtBack_OnClick(object sender, RoutedEventArgs e) //Переход на предыдущую страницу
         {
                 NavigationService?.Navigate(Cache.UserWorkMode);
-        }
-
-
-        private LoadingAnimationWindow loadingAnimationWindow;
-        private void ButtonNext_OnClick(object Sender, RoutedEventArgs E)
+        }        
+        
+        private void ButtonNext_OnClick(object sender, RoutedEventArgs e) //Переход на следующую страницу
         {
-            double left = Cache.Main.GetWaitProgressBarPoint.X + Cache.Main.Left;
-            double top = Cache.Main.GetWaitProgressBarPoint.Y + Cache.Main.Top;
-            double width = Cache.Main.GetWaitProgressBarSize.X;
-            double  height = Cache.Main.GetWaitProgressBarSize.Y;
-            Thread newWindowThread = new Thread(new ThreadStart(() =>
+            //Отображение анимации загрузки окна
+            double Left = Cache.Main.GetWaitProgressBarPoint.X + Cache.Main.Left;
+            double Top = Cache.Main.GetWaitProgressBarPoint.Y + Cache.Main.Top;
+            double Width = Cache.Main.GetWaitProgressBarSize.X;
+            double  Height = Cache.Main.GetWaitProgressBarSize.Y;
+            Thread NewWindowThread = new Thread(new ThreadStart(() =>
             {
-                loadingAnimationWindow = new WpfControlLibrary.CustomControls.LoadingAnimationWindow();
-                loadingAnimationWindow.Left = left;
-                loadingAnimationWindow.Top = top;
-                loadingAnimationWindow.Width = width;
-                loadingAnimationWindow.Height = height;
-                loadingAnimationWindow.Show();
-                System.Windows.Threading.Dispatcher.Run();
+                LoadingAnimationWindow = new LoadingAnimationWindow
+                {
+                    Left = Left,
+                    Top = Top,
+                    Width = Width,
+                    Height = Height
+                };
+                LoadingAnimationWindow.Show();
+                Dispatcher.Run();
             }));
-            newWindowThread.SetApartmentState(ApartmentState.STA);
-            newWindowThread.IsBackground = true;
-            newWindowThread.Start();
-
-
-
-            //            if (!Cache.Main.IsProfilesParsed)
-            //                ProfilesDbLogic.ImportProfilesFromDb();
-
-            if (tbPassword.Text == CurrentAccount.Password && NavigationService != null)
+            NewWindowThread.SetApartmentState(ApartmentState.STA);
+            NewWindowThread.IsBackground = true;
+            NewWindowThread.Start();
+            //Некорректный пароль
+            if (tbPassword.Text != CurrentAccount.Password || NavigationService == null)
             {
-                lblIncorrect.Content = "";
-                Cache.Main.VM.AccountName = CurrentAccount.Name;
-//                Cache.ProfileEdit.ClearFilter();
-//                Cache.ProfileSelection.InitFilter();
-//                Cache.ProfileSelection.InitSorting();
-                //NavigationService.Navigate(Cache.ProfileSelection);
-                PrepareMoveToSelectProfilePage(Cache.ProfilesPageSelectForTest);
-                Cache.ProfilesPageSelectForTest.AfterLoadAction -= ProfilesPageSelectForTest_AfterLoadAction;
-                Cache.ProfilesPageSelectForTest.AfterLoadAction += ProfilesPageSelectForTest_AfterLoadAction;
-                Debug.Assert(NavigationService != null, nameof(NavigationService) + " != null");
-                NavigationService.Navigate(Cache.ProfilesPageSelectForTest);
-            }
-            else
                 lblIncorrect.Content = Properties.Resources.PasswordIncorrect;
-
+                tbPassword.Text = string.Empty;
+                return;
+            }
+            lblIncorrect.Content = string.Empty;
+            Cache.Main.VM.AccountName = CurrentAccount.Name;
+            PrepareMoveToSelectProfilePage(Cache.ProfilesPageSelectForTest);
+            Cache.ProfilesPageSelectForTest.AfterLoadAction -= ProfilesPageSelectForTest_AfterLoadAction;
+            Cache.ProfilesPageSelectForTest.AfterLoadAction += ProfilesPageSelectForTest_AfterLoadAction;
+            Debug.Assert(NavigationService != null, nameof(NavigationService) + " != null");
+            NavigationService.Navigate(Cache.ProfilesPageSelectForTest);
             tbPassword.Text = string.Empty;
         }
 
-        private void ProfilesPageSelectForTest_AfterLoadAction()
+        private void ProfilesPageSelectForTest_AfterLoadAction() //Окончание анимации загрузки окна
         {
-            loadingAnimationWindow.Dispatcher.BeginInvoke(new Action(() =>
+            LoadingAnimationWindow.Dispatcher.BeginInvoke(new Action(() =>
             {
-                loadingAnimationWindow.Close();
+                LoadingAnimationWindow.Close();
             }));
         }
 
+        /// <summary>Подготовка к переходу на страницу профилей</summary>
+        /// <param name="profilesPage">Страница профилей</param>
         public static void PrepareMoveToSelectProfilePage(ProfilesPage profilesPage)
         {
             Cache.Main.VM.AccountNameIsVisibility = true;
             profilesPage.LoadTopProfiles();
-            profilesPage.Title = $"{Properties.Resources.AllProfiles}: {profilesPage.ProfileVm.Profiles.Count}";
-//            Cache.ProfilesPageSelectForTest.GoBackAction += () =>
-//            {
-//                var navigationService = Cache.ProfilesPageSelectForTest.NavigationService;
-//                Debug.Assert(navigationService != null, nameof(navigationService) + " != null");
-//                navigationService.Navigate(Cache.Login);
-//            };
+            profilesPage.Title = string.Format("{0}: {1}", Properties.Resources.AllProfiles, profilesPage.ProfileVm.Profiles.Count);
             profilesPage.ProfileVm.NextAction = () =>
             {
-                
                 Cache.UserTest.Profile = profilesPage.ProfileVm.SelectedProfile.ToProfile();
-                Cache.UserTest.Title = Properties.Resources.UserTestPage_Title + ", " + Properties.Resources.Profile.ToLower() + ": " + "\n" + Cache.UserTest.Profile;
-                //запоминаем в UserTest флаг 'Режим специальных измерений' для возможности корректной работы её MultiIdentificationFieldsToVisibilityConverter 
+                Cache.UserTest.Title = string.Format("{0}, {1}: \n{2}", Properties.Resources.UserTestPage_Title, Properties.Resources.Profile.ToLower(), Cache.UserTest.Profile);
                 Cache.UserTest.SpecialMeasureMode = (Cache.WorkMode == UserWorkMode.SpecialMeasure);
-
-                var navigationService = profilesPage.NavigationService;
-
+                NavigationService navigationService = profilesPage.NavigationService;
                 Cache.UserTest.InitSorting();
-                Cache.UserTest.InitTemp();
-                    
+                Cache.UserTest.InitTemp();                    
                 Debug.Assert(navigationService != null, "navigationService != null");
                 navigationService.Navigate(Cache.UserTest);
             };
-            
         }
 
-        private void AccountsListBox_OnSelectionChanged(object Sender, SelectionChangedEventArgs E)
+        private void AccountsListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) //Выбор в списке пользователей
         {
-            CurrentAccount = (CAccount) accountsListBox.SelectedItem;
-            lblIncorrect.Content = "";
+            CurrentAccount = (CAccount)accountsListBox.SelectedItem;
+            lblIncorrect.Content = string.Empty;
         }
-
-        private void LoginPage_Loaded(object Sender, RoutedEventArgs E)
-        {
-            lblIncorrect.Content = "";
-//            Cache.Main.VM.AccountName = string.Empty;
-        }
-
-        //private void TbPassword_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    if (WpfControlLibrary.Properties.Settings.Default.IsTouchUI && FindParent<Window>(this) is IMainWindow window)
-        //        window.ShowKeyboard(true, sender as );
-        //}
     }
 }
